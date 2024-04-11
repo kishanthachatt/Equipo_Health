f6a604b5444d61fc969434c12adecb72ab80675f
Kishan T <kishan.t@dbizsolution.com>
2024-04-11 20:04:05 +0530

    Updated the deploy file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { ROUTE } from "./Routes.constants";
 
@@ -10,7 +10,7 @@ import PageComingSoon from "./Routes/PageComingSoon/PageComingSoon.jsx";
 
 export function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path={"*"} element={<PageNotFound />} />
         <Route element={<Layout />}>
@@ -42,6 +42,6 @@ export function App() {
           />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
